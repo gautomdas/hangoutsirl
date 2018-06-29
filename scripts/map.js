86f89c8c3585a7e17bd2e37ead4261f5f5b5d47b
Zach Zhao <Zachzhao@outlook.com>
2018-06-29 00:20:39 -0400

    Buggy But works

--- scripts/map.js
+++ scripts/map.js
@@ -2,46 +2,88 @@ var locations = [];
 var input = document.getElementById("myInput");
 var listAddress = [];
 
-input.addEventListener("keyup", function(event) {
-    event.preventDefault();
-    if (event.keyCode === 13) {
-        document.getElementById("myBtn").click();
-    }
+$(".fa-times").click(function() {
+  var p = $(this).parents(".list-group-item");
+  p.fadeOut(500, function() {
+    $(this).css({
+      "visibility": "hidden",
+      display: 'block'
+    }).slideUp();
+  });
 });
 
-function addLoc(){
+
+
+input.addEventListener("keyup", function(event) {
+  event.preventDefault();
+  if (event.keyCode === 13) {
+    document.getElementById("myBtn").click();
+  }
+});
+
+function addLoc() {
   var location = document.getElementById("myInput").value;
   locations.push(location);
-  document.getElementById("addresses").innerHTML = locations;
+	addAddressItem("Bob", location);
+  //document.getElementById("addresses").innerHTML = locations;
+
   document.getElementById("myInput").value = '';
 
   /*get latitude longitude*/
   var geocoder = new google.maps.Geocoder();
-  console.log(locations[locations.length-1]);
-  var address = locations[locations.length-1];
+  console.log(locations[locations.length - 1]);
+  var address = locations[locations.length - 1];
 
-  geocoder.geocode( { 'address': address}, function(results, status) {
-  if (status == google.maps.GeocoderStatus.OK){
-      listAddress.push({lat:results[0].geometry.location.lat(), lng:results[0].geometry.location.lng()});
+  geocoder.geocode({
+    'address': address
+  }, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      listAddress.push({
+        lat: results[0].geometry.location.lat(),
+        lng: results[0].geometry.location.lng()
+      });
       initMap();
     }
   });
 }
 
+function addAddressItem(name, address) {
+  var itemHTML = `
+	<div href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+	<div class="d-flex w-100 justify-content-between">
+		<h5 class="mb-1">` + name + `</h5>
+		<a href = "#"><i class="fa fa-times" aria-hidden="true"></i></a>
+	</div>
+	<p class="mb-1">`+ address + `</p>
+	<small>`+ new Date().toJSON().slice(0,10).replace(/-/g,'/') + `</small>
+</div>`;
+console.log(itemHTML);
+$( "#itemAddress" ).append( $(itemHTML) );
+}
 
 function initMap() {
   /*initial list example*/
-  var cent = {lat: 38.907192, lng: -77.036871}
-  var map = new google.maps.Map(document.getElementById('map'), {zoom: 10, center: cent});
+  var cent = {
+    lat: 38.907192,
+    lng: -77.036871
+  }
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: cent
+  });
   text = "<ul>";
 
-  for(i=0; i<listAddress.length;i++){
-    var marker = new google.maps.Marker({position: listAddress[i], map: map});
+  for (i = 0; i < listAddress.length; i++) {
+    var marker = new google.maps.Marker({
+      position: listAddress[i],
+      map: map
+    });
     text += "<li>" + listAddress[i] + "</li>";
   }
   text += "</ul>";
   document.getElementById("demo").innerHTML = text;
 
+	addressItem("zach", "123 Main St.")
   /*active search bar part*/
   var input = document.getElementById('myInput');
   var searchBox = new google.maps.places.SearchBox(input);
@@ -60,7 +102,9 @@ function initMap() {
       position: google.maps.ControlPosition.TOP_CENTER,
       drawingModes: ['marker', 'circle', 'polygon', 'polyline', 'rectangle']
     },
-    markerOptions: {icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'},
+    markerOptions: {
+      icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
+    },
     circleOptions: {
       fillColor: '#ffff00',
       fillOpacity: 1,
